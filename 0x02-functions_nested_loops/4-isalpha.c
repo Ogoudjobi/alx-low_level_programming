45dc56520ab93fd539f3ad36766f85643eab7fdc
Ogoudjobi <cephas.attere@gmail.com>
2022-03-16 01:15:51 -0700

    4. isalpha && correction of bug

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabet character
+ * @c: character to check
+ * Return: 1 if alphanet character, 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
